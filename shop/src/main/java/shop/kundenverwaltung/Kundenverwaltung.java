533c476e3fa34f12e0cae7105eebd83793ea6e0e
Andreas Jankowoi <jaan1011(at)hs-karlsruhe.de>
2013-10-30 16:57:49 +0100

    Kundenverwaltung hinzugefügt

---
+++ shop/src/main/java/shop/kundenverwaltung/Kundenverwaltung.java
@@ -0,0 +1,72 @@
+package shop.kundenverwaltung;
+
+import java.util.List;
+
+
+
+public class Kundenverwaltung {
+	
+	
+
+	public List<Kunde> findAllKunde() {
+		return null;
+	}
+	
+	public List<Kunde> findKundeByTitel() {
+		return null;
+	
+	}
+	
+	public Kunde findKundeById() {
+		return null;
+		
+	}
+
+	public Kunde findKundeByName() {
+		return null;
+		
+	}
+
+	public List<Kunde> findKundeByStadt() {
+		return null;
+		
+	}
+
+
+	public List<Kunde> findKundeByVorname() {
+		return null;
+	
+	}
+
+	public List<Kunde> findKundeByGeburtstag() {
+		return null;
+	
+	}
+	public List<Kunde> findKundeByStrasse() {
+		return null;
+	
+	}
+	public List<Kunde> findKundeByPlz() {
+		return null;
+	
+	}
+
+	public Kunde createKunde() {
+		return null;
+		
+	}
+
+	public Kunde updateKunde(Kunde kunde) {
+		return kunde;
+	
+	}
+
+	public void deleteKunde(Kunde kunde) {
+	
+	}
+
+	public void deleteKundeById(Long id) {
+
+	}
+	
+}
