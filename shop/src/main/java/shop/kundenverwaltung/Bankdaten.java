767b1a911a4438a66336a7686bd3a234fe99b766
Patrick Gabor <gaborpatrick(at)yahoo.de>
2013-10-30 15:51:02 +0100

    klasse kunde und bankdaten hinzugefügt

---
+++ shop/src/main/java/shop/kundenverwaltung/Bankdaten.java
@@ -0,0 +1,63 @@
+package shop.kundenverwaltung;
+
+public class Bankdaten {
+	
+	private String bankname;
+	private int blz;
+	private int kontonummer;
+	public String getBankname() {
+		return bankname;
+	}
+	public void setBankname(String bankname) {
+		this.bankname = bankname;
+	}
+	public int getBlz() {
+		return blz;
+	}
+	public void setBlz(int blz) {
+		this.blz = blz;
+	}
+	public int getKontonummer() {
+		return kontonummer;
+	}
+	public void setKontonummer(int kontonummer) {
+		this.kontonummer = kontonummer;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((bankname == null) ? 0 : bankname.hashCode());
+		result = prime * result + blz;
+		result = prime * result + kontonummer;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Bankdaten other = (Bankdaten) obj;
+		if (bankname == null) {
+			if (other.bankname != null)
+				return false;
+		} else if (!bankname.equals(other.bankname))
+			return false;
+		if (blz != other.blz)
+			return false;
+		if (kontonummer != other.kontonummer)
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Bankdaten [bankname=" + bankname + ", blz=" + blz
+				+ ", kontonummer=" + kontonummer + "]";
+	}
+	
+
+}
--- shop/src/main/java/shop/kundenverwaltung/Kunde.java
+++ shop/src/main/java/shop/kundenverwaltung/Kunde.java
@@ -1,5 +1,99 @@
 package shop.kundenverwaltung;
+
+
+
 public class Kunde {
 	private Long id;
-	private int tel;
+	private String titel;
+	private String vorname;
+	private String zweitvorname;
+	private String nachname;
+	private String geburtstag;
+	private String strasse;
+	private String plz;
+	private String stadt;
+	private Long telefon;
+	private String email;
+	private Bankdaten  bankdaten;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitel() {
+		return titel;
+	}
+	public void setTitel(String titel) {
+		this.titel = titel;
+	}
+	public String getVorname() {
+		return vorname;
+	}
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+	public String getZweitvorname() {
+		return zweitvorname;
+	}
+	public void setZweitvorname(String zweitvorname) {
+		this.zweitvorname = zweitvorname;
+	}
+	public String getNachname() {
+		return nachname;
+	}
+	public void setNachname(String nachname) {
+		this.nachname = nachname;
+	}
+	public String getGeburtstag() {
+		return geburtstag;
+	}
+	public void setGeburtstag(String geburtstag) {
+		this.geburtstag = geburtstag;
+	}
+	public String getStrasse() {
+		return strasse;
+	}
+	public void setStrasse(String strasse) {
+		this.strasse = strasse;
+	}
+	public String getPlz() {
+		return plz;
+	}
+	public void setPlz(String plz) {
+		this.plz = plz;
+	}
+	public String getStadt() {
+		return stadt;
+	}
+	public void setStadt(String stadt) {
+		this.stadt = stadt;
+	}
+	public Long getTelefon() {
+		return telefon;
+	}
+	public void setTelefon(Long telefon) {
+		this.telefon = telefon;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Bankdaten getBankdaten() {
+		return bankdaten;
+	}
+	public void setBankdaten(Bankdaten bankdaten) {
+		this.bankdaten = bankdaten;
+	}
+	@Override
+	public String toString() {
+		return "Kunde [id=" + id + ", titel=" + titel + ", vorname=" + vorname
+				+ ", zweitvorname=" + zweitvorname + ", nachname=" + nachname
+				+ ", geburtstag=" + geburtstag + ", strasse=" + strasse
+				+ ", plz=" + plz + ", stadt=" + stadt + ", telefon=" + telefon
+				+ ", email=" + email + ", bankdaten=" + bankdaten + "]";
+	}
 }
