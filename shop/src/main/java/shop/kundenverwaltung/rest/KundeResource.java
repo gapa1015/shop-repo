7b16a36409f26f6330fe35de906eedbc6abe90c8
Andreas Jankowoi <jaan1011(at)hs-karlsruhe.de>
2013-11-04 01:09:40 +0100

    GET angefangen zu machen

---
+++ shop/src/main/java/shop/kundenverwaltung/rest/KundeResource.java
@@ -0,0 +1,48 @@
+package shop.kundenverwaltung.rest;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.MediaType.APPLICATION_XML;
+import static javax.ws.rs.core.MediaType.TEXT_XML;
+
+import javax.inject.Inject;
+import javax.websocket.server.PathParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+
+
+
+import shop.util.UriHelper;
+
+
+@Path("/kunden")
+@Produces({ APPLICATION_JSON, APPLICATION_XML + ";qs=0.75", TEXT_XML + ";qs=0.5" })
+@Consumes
+public class KundeResource {
+	
+	@Context
+	private UriInfo uriInfo;
+	@Inject
+	private UriHelper uriHelper;
+	
+	@GET
+	@Path("{id:[1-9] [0-9]*}")
+	public Response findKundeById(@PathParam("id") Long id) {
+		
+		
+		
+		return null; //hab GET für Kunde angefangen
+		
+	}
+		
+	
+	
+	
+	
+
+}
