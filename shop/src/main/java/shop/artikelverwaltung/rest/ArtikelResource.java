7f67d2e44d60e1b5ac755c4f8c92547e586488ab
Patrick Gabor <gaborpatrick(at)yahoo.de>
2013-11-06 19:29:48 +0100

    ArtikelResource überarbeitet, Bestellresource überarbeitet, Mock-Datei ergänzt Package Artikelverwaltung.Service geleert

--- shop/src/main/java/shop/artikelverwaltung/rest/ArtikelResource.java
+++ shop/src/main/java/shop/artikelverwaltung/rest/ArtikelResource.java
@@ -3,6 +3,7 @@ package shop.artikelverwaltung.rest;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static javax.ws.rs.core.MediaType.APPLICATION_XML;
 import static javax.ws.rs.core.MediaType.TEXT_XML;
+import static shop.util.Constants.SELF_LINK;
 
 import java.net.URI;
 
@@ -17,6 +18,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Link;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
@@ -37,17 +39,26 @@ public class ArtikelResource {
 
 	@GET
 	@Path("{id:[1-9][0-9]*}")
-	public Rad findRadById(@PathParam("id") Long id) {
-		final Rad rad = Mock.findeRadById(id);
+	public Response findRadById(@PathParam("id") Long id) {
+		final Rad rad = Mock.findRadById(id);
 		if (rad == null) {
-			throw new NotFoundException("Kein Fahrrad mit der ID " + id
-					+ " gefunden.");
+			throw new NotFoundException("Kein Rad mit der ID " + id + " gefunden.");
 		}
-		return rad;
+		
+		return Response.ok(rad)
+					   .links(getTransitionalLinks(rad, uriInfo))
+					   .build();
+	}
+	
+	private Link[] getTransitionalLinks(Rad rad, UriInfo uriInfo) {
+		final Link self = Link.fromUri(getUriRad(rad, uriInfo))
+							  .rel(SELF_LINK)
+							  .build();
+		return new Link[] { self };
 	}
 
 	public URI getUriRad(Rad rad, UriInfo uriInfo) {
-		return uriHelper.getURI(ArtikelResource.class, "findeRadById",
+		return uriHelper.getURI(ArtikelResource.class, "findRadById",
 				rad.getId(), uriInfo);
 	}
 
@@ -60,14 +71,14 @@ public class ArtikelResource {
 	}
 
 	@PUT
-	@Produces({ APPLICATION_JSON, APPLICATION_XML, TEXT_XML })
-	@Consumes
+	@Consumes({APPLICATION_JSON, APPLICATION_XML, TEXT_XML })
+	@Produces
 	public void updateRad(Rad rad) {
 		Mock.updateRad(rad);
 	}
 
 	@DELETE
-	@Path("{id:[1-9] [0-9]*}")
+	@Path("{id:[1-9][0-9]*}")
 	@Produces
 	public void deleteRad(@PathParam("id") Long id) {
 		Mock.deleteRad(id);
