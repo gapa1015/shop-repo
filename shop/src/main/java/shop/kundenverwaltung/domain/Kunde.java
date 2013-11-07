1e832d91fe70fc56782c617018d6ca823e91c8c5
Daniel Hauth <Student@Student-PC>
2013-11-07 17:30:12 +0100

    stylecheck kunde

--- shop/src/main/java/shop/kundenverwaltung/domain/Kunde.java
+++ shop/src/main/java/shop/kundenverwaltung/domain/Kunde.java
@@ -2,10 +2,6 @@ package shop.kundenverwaltung.domain;
 
 import java.util.Date;
 
-
-
-
-
 public class Kunde {
 	private Long id;
 	private String vorname;
@@ -14,56 +10,72 @@ public class Kunde {
 	private Date geburtstag;
 	private String telefon;
 	private String email;
-	private Bankdaten  bankdaten;
-	
+	private Bankdaten bankdaten;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getVorname() {
 		return vorname;
 	}
+
 	public void setVorname(String vorname) {
 		this.vorname = vorname;
 	}
+
 	public String getNachname() {
 		return nachname;
 	}
+
 	public void setNachname(String nachname) {
 		this.nachname = nachname;
 	}
+
 	public Date getGeburtstag() {
 		return geburtstag;
 	}
+
 	public void setGeburtstag(Date geburtstag) {
 		this.geburtstag = geburtstag;
 	}
+
 	public String getTelefon() {
 		return telefon;
 	}
+
 	public void setTelefon(String telefon) {
 		this.telefon = telefon;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public Bankdaten getBankdaten() {
 		return bankdaten;
 	}
+
 	public void setBankdaten(Bankdaten bankdaten) {
 		this.bankdaten = bankdaten;
 	}
+
 	public Adresse getAdresse() {
 		return adresse;
 	}
+
 	public void setAdresse(Adresse adresse) {
 		this.adresse = adresse;
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -81,6 +93,7 @@ public class Kunde {
 		result = prime * result + ((vorname == null) ? 0 : vorname.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -93,45 +106,54 @@ public class Kunde {
 		if (adresse == null) {
 			if (other.adresse != null)
 				return false;
-		} else if (!adresse.equals(other.adresse))
+		}
+		else if (!adresse.equals(other.adresse))
 			return false;
 		if (bankdaten == null) {
 			if (other.bankdaten != null)
 				return false;
-		} else if (!bankdaten.equals(other.bankdaten))
+		}
+		else if (!bankdaten.equals(other.bankdaten))
 			return false;
 		if (email == null) {
 			if (other.email != null)
 				return false;
-		} else if (!email.equals(other.email))
+		}
+		else if (!email.equals(other.email))
 			return false;
 		if (geburtstag == null) {
 			if (other.geburtstag != null)
 				return false;
-		} else if (!geburtstag.equals(other.geburtstag))
+		} 
+		else if (!geburtstag.equals(other.geburtstag))
 			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
-		} else if (!id.equals(other.id))
+		} 
+		else if (!id.equals(other.id))
 			return false;
 		if (nachname == null) {
 			if (other.nachname != null)
 				return false;
-		} else if (!nachname.equals(other.nachname))
+		} 
+		else if (!nachname.equals(other.nachname))
 			return false;
 		if (telefon == null) {
 			if (other.telefon != null)
 				return false;
-		} else if (!telefon.equals(other.telefon))
+		} 
+		else if (!telefon.equals(other.telefon))
 			return false;
 		if (vorname == null) {
 			if (other.vorname != null)
 				return false;
-		} else if (!vorname.equals(other.vorname))
+		} 
+		else if (!vorname.equals(other.vorname))
 			return false;
 		return true;
 	}
+
 	@Override
 	public String toString() {
 		return "Kunde [id=" + id + ", vorname=" + vorname + ", nachname="
