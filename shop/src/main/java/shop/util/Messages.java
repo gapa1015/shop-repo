d12aeb2dec67ca897ef116868a27000dbbc3df1d
Andreas Jankowoi <jaan1011(at)hs-karlsruhe.de>
2013-12-08 16:08:11 +0100

    Messegesklasse fertig , implementiert in der Web.xml

---
+++ shop/src/main/java/shop/util/Messages.java
@@ -0,0 +1,99 @@
+package shop.util;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.core.HttpHeaders;
+
+import com.google.common.base.Splitter;
+
+@ApplicationScoped
+public class Messages {
+	
+	private static final String APPLICATION_MESSAGES ="/ApplicationMessages";
+	private static final List<Locale> LOCALES_DEFAULT = Arrays.asList(Locale.ENGLISH);
+	
+	@Resource(name = "locales")
+	private String locales;
+	
+	private ResourceBundle defaultBundle;
+	
+	private Map<Locale, ResourceBundle> bundles;
+	
+	private Map<String, ResourceBundle>bundlesLanguageStr;
+	
+	@PostConstruct
+	private void postConstruct() {
+		List<Locale>localesList;
+		if(locales==null)
+			localesList= LOCALES_DEFAULT;
+		else {
+			localesList = new ArrayList<>();
+			Iterable<String> localesIter = Splitter.on(',')
+													.trimResults()
+													.omitEmptyStrings()
+													.split(locales);
+			Locale.Builder localeBuilder = new Locale.Builder();
+			for (String localeStr : localesIter) {
+				localeBuilder.setLanguage(localeStr);
+				localesList.add(localeBuilder.build());
+			}
+		}
+		
+		bundles = new HashMap<>();
+		bundlesLanguageStr = new HashMap<>();
+		Set<String> languages =new HashSet<>();
+		for(Locale locale : localesList) {
+			ResourceBundle bundle = ResourceBundle.getBundle(APPLICATION_MESSAGES, locale);
+			bundles.put(locale, bundle);
+			
+			String localeStr = locale.toString();
+			if(localeStr.length() > 2) {
+				localeStr = localeStr.substring(0, 2);
+				if(!languages.contains(localeStr)) {
+					bundlesLanguageStr.put(localeStr, bundle);
+					languages.add(localeStr);
+				}
+			}
+		}
+		defaultBundle = bundles.get(localesList.get(0));
+	}
+	
+	public String getMessage(HttpHeaders headers, String Key, Object... args) {
+		List<Locale> locales = headers.getAcceptableLanguages();
+		ResourceBundle bundle = getBundle(locales);
+		
+		String pattern = bundle.getString(Key);
+		MessageFormat messageFormat= new MessageFormat(pattern, locales.get(0));
+		return messageFormat.format(args);
+		
+	}
+	
+	private ResourceBundle getBundle(List<Locale>locales) {
+		ResourceBundle bundle = null;
+		for(Locale locale : locales) {
+			bundle = bundles.get(locale);
+			if(bundle !=null)
+				break;
+			String localeStr = locale.toString();
+			if(localeStr.length()>2) {
+				localeStr = localeStr.substring(0, 2);
+				bundle = bundlesLanguageStr.get(localeStr);
+				if(bundle != null)
+					break;
+			}
+		}
+		return bundle == null ? defaultBundle : bundle;
+	}
+}
