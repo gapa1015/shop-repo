855a17ad7c7bb2c4983315d3d90ce49290003b93
Patrick Gabor <gaborpatrick(at)yahoo.de>
2013-12-09 20:46:21 +0100

    KundeService und ArtikelService CDI-fähig gemacht

--- shop/src/main/java/shop/artikelverwaltung/service/ArtikelServiceImp.java
+++ shop/src/main/java/shop/artikelverwaltung/service/ArtikelServiceImp.java
@@ -2,14 +2,17 @@ package shop.artikelverwaltung.service;
 
 import java.io.Serializable;
 
+import javax.enterprise.context.Dependent;
 import javax.validation.constraints.NotNull;
 
 import shop.artikelverwaltung.domain.Rad;
 import shop.util.Mock;
+import shop.util.interceptor.Log;
 
+@Dependent
+@Log
 public class ArtikelServiceImp implements ArtikelService, Serializable{
-
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 3471955805910643313L;
 
 	@Override
 	@NotNull(message = "{artikel.id.notNull}")
--- shop/src/main/java/shop/kundenverwaltung/service/KundenService.java
+++ shop/src/main/java/shop/kundenverwaltung/service/KundenService.java
@@ -2,12 +2,17 @@ package shop.kundenverwaltung.service;
 
 import java.io.Serializable;
 
+import javax.enterprise.context.Dependent;
+
 import shop.kundenverwaltung.domain.AbstractKunde;
 import shop.util.Mock;
+import shop.util.interceptor.Log;
 
+@Dependent
+@Log
 public class KundenService implements Serializable {
-	
-	
+	private static final long serialVersionUID = -4188395218729678116L;
+
 	public AbstractKunde findKundeById(Long id) {
 		if (id == null)
 			return null;
