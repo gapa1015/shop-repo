5e96fd28ae9af5f6b866707fcd50350ec86a72cc
Patrick Gabor <gaborpatrick@yahoo.de>
2014-01-03 13:47:28 +0100

    Klasse AbstractAuditable in util.persistence erstellt und bearbeitet

---
+++ shop/src/main/java/shop/util/persistence/AbstractAuditable.java
@@ -0,0 +1,60 @@
+package shop.util.persistence;
+
+import static javax.persistence.TemporalType.TIMESTAMP;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Basic;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Temporal;
+import javax.xml.bind.annotation.XmlTransient;
+
+@MappedSuperclass
+public abstract class AbstractAuditable implements Serializable {
+	private static final long serialVersionUID = 1158882637171201007L;
+
+	@Temporal(TIMESTAMP)
+	@Basic(optional = false)
+	@XmlTransient
+	private Date erzeugt;
+
+	@Temporal(TIMESTAMP)
+	@Basic(optional = false)
+	@XmlTransient
+	private Date aktualisiert;
+	
+	@PrePersist
+	protected void prePersist() {
+		erzeugt = new Date();
+		aktualisiert = new Date();
+	}
+	
+	@PreUpdate
+	protected void preUpdate() {
+		aktualisiert = new Date();
+	}
+	
+	public Date getErzeugt() {
+		return erzeugt == null ? null : (Date) erzeugt.clone();
+	}
+
+	public void setErzeugt(Date erzeugt) {
+		this.erzeugt = erzeugt == null ? null : (Date) erzeugt.clone();
+	}
+
+	public Date getAktualisiert() {
+		return aktualisiert == null ? null : (Date) aktualisiert.clone();
+	}
+
+	public void setAktualisiert(Date aktualisiert) {
+		this.aktualisiert = aktualisiert == null ? null : (Date) aktualisiert.clone();
+	}
+
+	@Override
+	public String toString() {
+		return "AbstractAuditable [erzeugt=" + erzeugt + ", aktualisiert=" + aktualisiert + "]";
+	}
+}
