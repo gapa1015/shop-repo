c4bc58fe391a2e1fb4c29f767843cf5063995996
Patrick Gabor <gaborpatrick@yahoo.de>
2014-01-06 11:46:28 +0100

    EntityManagerProducer

---
+++ shop/src/main/java/shop/util/persistence/EntityManagerProducer.java
@@ -0,0 +1,13 @@
+package shop.util.persistence;
+
+import javax.enterprise.context.Dependent;
+import javax.enterprise.inject.Produces;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Dependent
+public class EntityManagerProducer {
+	@PersistenceContext
+	@Produces
+	private EntityManager em;
+}
